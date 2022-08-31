57c11fc7a02eef3b667d06dbbef07185a7591a7a
thanos <thanecdsmith@gmail.com>
2022-08-31 15:32:06 +0100

    streamlined releases

--- zip_releases.sh
+++ zip_releases.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-rm release/*
-zip -r release/osmp-pack assets pack.png pack.mcmeta
+mv release/*.zip release/old/$(date +"osmp-pack-%F_%T.zip")
+zip -r release/osmp-pack.zip assets pack.png pack.mcmeta
+sha1sum release/*.zip
